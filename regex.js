7c5c9c50b8011b53f56878df60eeacabc551d6e9
EricLOsorio <erioso@verizon.net>
2017-01-13 15:57:41 -0500

    Update regex.js

--- regex.js
+++ regex.js
@@ -4,7 +4,9 @@ var parseButton=document.getElementById('button');
 var stringInput=document.getElementById('inputString');
 var harvestedLinks=document.getElementById('output');
 
+
 function parseLinks(text){
+
   var arr=[];
   var pattern0=/<a\s*(href=")(.*?)<\/a>/g;
   var links=text.split('<a href="');
@@ -32,10 +34,10 @@ function parseLinks(text){
     rawLink[j]=arr[j][0];
     rawLinkPattern=/"[\S*\s*]*"/;
     urlString=rawLinkPattern.exec(rawLink[j])[0];
-    urlPattern=/[^"][\S*\s*]*[^"]/;
+    urlPattern=/["](.*?)["]/;
     textLinkPattern=/>(.*?)</;
     textLinks[j]=textLinkPattern.exec(rawLink[j])[0].slice(1,textLinkPattern.exec(rawLink[j])[0].length-1);
-    url[j]=urlPattern.exec(urlString)[0];
+    url[j]=urlPattern.exec(urlString)[0].slice(1,urlPattern.exec(rawLink[j])[0].length-1);
   };
 
   for(var k=0;k<url.length;k++){
@@ -51,12 +53,15 @@ function parseLinks(text){
 
   linksAndEmailsArray=[wwwLinkText,wwwUrl,emailAddress]
 
-  objString='{"links":[{';
-  for(var i=0;i<wwwUrl.length;i++){
-    objString+='"linkText":'+'"'+wwwLinkText[i]+'"'+',';
-    objString+='"url":'+'"'+wwwUrl[i]+'"}';
-    if(i<wwwUrl.length-1){
-      objString+=',{';
+  objString='{"links":[';
+  if(wwwUrl.length !==0){
+    objString+='{';
+    for(var i=0;i<wwwUrl.length;i++){
+      objString+='"linkText":'+'"'+wwwLinkText[i]+'"'+',';
+      objString+='"url":'+'"'+wwwUrl[i]+'"}';
+      if(i<wwwUrl.length-1){
+        objString+=',{';
+      };
     };
   };
   objString+='], "emailAddresses":[';
@@ -86,7 +91,6 @@ parseButton.addEventListener('click', function(){
   var linkNum=0;
   var emailNum=0;
 
-  
   if(numOfLinks!==0){
     for(var i=0;i<numOfLinks;i++){
       linkNum=i+1;
@@ -106,7 +110,6 @@ if(numOfeMailAddresses===0 && numOfLinks===0){
   harvestedLinks.value="No External Links or Email Addresses exist in The Input String";
 };
 
-
   output.className+="focused";
 
 
@@ -114,4 +117,5 @@ if(numOfeMailAddresses===0 && numOfLinks===0){
 
 
 });
+
 
